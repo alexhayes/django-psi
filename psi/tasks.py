925562eb452d84ca0a9fdb495d76eb484fc51e31
Alex Hayes <alex.hayes@roi.com.au>
2015-05-11 12:28:16 +1000

    No longer ignoring result on populate_page_insight

--- psi/tasks.py
+++ psi/tasks.py
@@ -3,7 +3,7 @@ from celery import task
 from django.conf import settings
 
 
-@task(ignore_result=True, queue=settings.PSI_CELERY_QUEUE)
+@task(queue=settings.PSI_CELERY_QUEUE)
 def populate_page_insight(pk):
     page_insight = PageInsight.objects.get(pk=pk)
     page_insight.populate()
